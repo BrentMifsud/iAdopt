3d2f33d21885b6055a5013c9b4b697c72271f14a
Brent Mifsud <brentmifsud@icloud.com>
2020-05-02 23:23:12 -0400

    add dummy unit test to build

---
+++ iAdoptUnitTests/IAdoptUnitTest.swift
@@ -0,0 +1,33 @@
+//
+//  IAdoptUnitTest.swift
+//  iAdoptUnitTests
+//
+//  Created by Brent Mifsud on 2020-05-02.
+//  Copyright © 2020 Brent Mifsud. All rights reserved.
+//
+
+import XCTest
+
+class IAdoptUnitTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
